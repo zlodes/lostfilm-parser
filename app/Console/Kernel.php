7c6f5fc544409f2886bb7f72b5cb3eadb475dd75
Zlatoslav <px.zlodes@gmail.com>
2019-01-13 20:32:29 +0300

    Scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('lostfilm:crawl-news-page')
+            ->daily()
+            ->withoutOverlapping();
     }
 
     /**
