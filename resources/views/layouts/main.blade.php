9031324f7683a09025309bc20977aa1a2a712839
Zlatoslav <px.zlodes@gmail.com>
2019-01-13 17:33:56 +0300

    Back to Bootstrap 3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,29 +7,35 @@
     <title>{{ config('app.name') }}</title>
 
     {{-- Styles --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+          integrity="sha384-PmY9l28YgO4JwMKbTvgaS7XNZJ30MK9FAZjjzXtlqyZCqBY6X6bXIkM++IkyinN+" crossorigin="anonymous">
 </head>
 <body>
 <header id="site-header">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    {{-- Here be app navigation --}}
-                </ul>
-                <form class="form-inline my-2 my-lg-0" method="GET" action="{{ action('SeriesController@index') }}">
-                    <input class="form-control mr-sm-2" type="search" name="q" placeholder="Поиск" aria-label="Поиск" value="{{ $query ?? '' }}">
-
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Искать</button>
-                </form>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
             </div>
-        </nav>
-    </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-right" action="{{ action('SeriesController@index') }}" method="get">
+                    <div class="form-group">
+                        <input type="search" class="form-control" name="q" placeholder="Поиск" value="{{ $query ?? '' }}">
+                    </div>
+                    <button type="submit" class="btn btn-default">Искать</button>
+                </form>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 </header>
 
 <section id="main" class="content-wrapper">
@@ -41,8 +47,10 @@
 </footer>
 
 {{-- Scripts --}}
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"
+        integrity="sha384-vhJnz1OVIdLktyixHY4Uk3OHEwdQqPppqYR8+5mjsauETgLOcEynD9oPHhhz18Nw" crossorigin="anonymous"></script>
 
 </body>
 </html>
